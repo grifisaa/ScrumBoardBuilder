37defff8deee09ea098e5222cd0ca24f4b9d9e62
Coby Garner <cobyone12345@gmail.com>
2022-03-30 20:01:09 -0600

    Reorganized project structure

---
+++ client/src/main/java/client/App.java
@@ -0,0 +1,38 @@
+package client;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+import javafx.application.Application;
+
+/**
+ * author: Sushan Manandhar
+ */
+
+public class App extends Application {
+    Stage stage;
+    public App(){}
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        this.stage = stage;
+        stage.setTitle("Scrumbags!!!");
+        showHome();
+        stage.show();
+    }
+
+    public void showHome() throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setController(new HelloController(this));
+        loader.setLocation(getClass().getResource("/mainBoard.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+    }
+
+    public static void main(String[] args) {
+        App.launch();
+    }
+}
---
+++ client/src/main/java/client/HelloController.java
@@ -0,0 +1,19 @@
+package client;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    private App app;
+
+    public HelloController(App app){
+        this.app = app;
+    };
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
