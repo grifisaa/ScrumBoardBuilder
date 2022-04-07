94b3a1ed04c4ba4ab943e6a91a0a30b04b3c1662
manasush <46699115+manasush@users.noreply.github.com>
2022-04-06 21:02:13 -0600

    Added User Story
    Still working on Draggable

--- clientfrontend/ScrumTool/src/main/java/com/example/scrumtool/HelloApplication.java
+++ clientfrontend/ScrumTool/src/main/java/com/example/scrumtool/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.scrumtool;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,11 +12,19 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("mainBoard.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+
+
+
+        //////////////////////////////
+        Button button = new Button();
+
+
+        Scene scene = new Scene(fxmlLoader.load(), 900, 900);
         stage.setTitle("ScrumBag!!!");
         stage.setScene(scene);
         stage.show();
     }
+//        root.setCenter(button);
 
     public static void main(String[] args) {
         launch();
--- clientfrontend/ScrumTool/src/main/java/com/example/scrumtool/HelloController.java
+++ clientfrontend/ScrumTool/src/main/java/com/example/scrumtool/HelloController.java
@@ -1,14 +1,37 @@
 package com.example.scrumtool;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Rectangle;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+public class HelloController implements Initializable{
+//    @FXML
+//    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Rectangle rectangle;
+
+    @FXML
+    private AnchorPane anchorPane;
+
+
+
+    DraggableMaker draggableMaker = new DraggableMaker();
+
+//    @FXML
+//    protected void onHelloButtonClick() {
+//        welcomeText.setText("Welcome to JavaFX Application!");
+//    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        draggableMaker.makeDraggable(rectangle);
+        draggableMaker.makeDraggable(anchorPane);
     }
+
 }
